344a22743f9604a7c1f15986a4a7d31baf575572
RicardoFM1 <ricardofernandesdemoura7@gmail.com>
2025-08-01 03:30:46 -0300

    filtro

--- src/componentes/counteudos/resumoDiv/resumoDiv.tsx
+++ src/componentes/counteudos/resumoDiv/resumoDiv.tsx
@@ -3,21 +3,31 @@ import { apiResVendasResumoGet } from "../../apiRes/apiResVendas";
 import { Iconify } from "../../iconify/iconify";
 import style from "./resumoDiv.module.css";
 import type { iResumo, ResumoDivProps } from "../../Interfaces/resumoInterface";
+import { apiResProdutoGet } from "../../apiRes/apiResProdutos";
+import type { iProduto } from "../../Interfaces/produtoDivInterface";
 
 
 export const Resumo = ({ divType }: ResumoDivProps) => {
   const [resumo, setResumo] = useState<iResumo | null>(null);
+  const [produtos, setProdutos] = useState([])
   const getResumo = async () => {
     const apiRes = await apiResVendasResumoGet();
     setResumo(apiRes.data);
   };
 
+   const getProduto = async () => {
+    const apiRes = await apiResProdutoGet();
+    setProdutos(apiRes.data);
+  };
+
   useEffect(() => {
     getResumo();
+    getProduto();
   }, []);
 
   useEffect(() => {
     setTimeout(() => {
+      console.log(produtos)
       console.log(resumo)
     }, 3000);
   }, [resumo]);
@@ -26,6 +36,7 @@ export const Resumo = ({ divType }: ResumoDivProps) => {
     <div className={style.resumoHoje}>
       {divType === "Resumo" ? (
         <div className={style.Resumo}>
+          <div className={style.headerResumo}>
           <h2>
             <Iconify
               className={style.graph}
@@ -35,6 +46,12 @@ export const Resumo = ({ divType }: ResumoDivProps) => {
             />{" "}
             Resumo
           </h2>
+          <select className={style.selectProdutos} name="opcoes" >
+            {produtos.map((produto:iProduto) =>
+            <option key={produto.id} value={produto.id}>{produto.name}</option>
+        )}
+          </select>
+          </div>
         
              
           <div className={style.caixaStatus}>
